f324bf67ca1df47291a7782344cf000523268314
Felicia Ahlström <fellan@MacBook-Air-som-tillhor-Felicia.local>
2023-05-14 20:44:30 +0200

    added two arrays with questions and choices

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,12 +2,66 @@ import {useState} from "react";
 import '../styles/style.css';
 const Quiz = () => {
     const [activeQuestion, setActiveQuestion] = useState(0);
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
+    const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
+    const [result, setResult] = useState([]);
+
+    const questions = [
+        {
+            question: 'What are you doing?'
+        },
+        {
+            question: 'Huh.'
+        },
+        {
+            question: 'Me?'
+        },
+    ];
+
+    const choices = [
+        {
+          id: 0,
+          name: 'Ante',
+        },
+        {
+            id: 1,
+            name: 'Me?',
+        },
+        {
+            id: 2,
+            name: 'Fellan',
+        },
+        {
+            id: 3,
+            name: 'Huh',
+        },
+    ];
+
+    console.log(questions);
 
     const onClickNext = () => {
         setActiveQuestion(prevActiveQuestion => prevActiveQuestion + 1);
         const nextQuestionIndex = activeQuestion + 1;
 
         setActiveQuestion(nextQuestionIndex);
+        setSelectedAnswer(null);
+        setSelectedAnswerIndex(null);
+    }
+
+    //sätter vilket svar som valts
+    const onSelectedAnswer = (answer, index) => {
+        setSelectedAnswer(answer);
+        setSelectedAnswerIndex(index);
+        //sparar och kopierar alla properties av det tidigare resultatet och använder spread operator (...prevResult)
+        //activeQuestion agerar "key" för att veta vilken fråga svaret ska sparas
+
+        setResult(prevResult => ({
+            ...prevResult,
+            [activeQuestion]: {
+                id: choices[index].id,
+                name: choices[index].name,
+            }
+        }));
     }
 
 
@@ -20,10 +74,12 @@ const Quiz = () => {
                     </div>
                 </div>
                 <div className="question-container">
-                    <span className="question">Question</span>
+                    <span className="question">{`${questions[activeQuestion].question}`}</span>
                 </div>
                 <div className="container">
-                    <div id="option-div">Choice</div>
+                    {choices.map((choice, index) => (
+                        <div id="option-div" key={index}>{`${choice.name}`}</div>
+                    ))}
                 </div>
                 <button className="next-button" onClick={onClickNext}>{'Next'}</button>
             </div>
